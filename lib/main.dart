06fff03ca72a4a638509208345e34a289644deaf
toughbox <nowonstation@gmail.com>
2025-06-23 22:43:40 +0900

    세로고정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'constants/colors.dart';
@@ -14,6 +15,10 @@ import 'screens/splash_screen.dart';
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  //화면 세로고정
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const SuperDayApp());
 }
 
